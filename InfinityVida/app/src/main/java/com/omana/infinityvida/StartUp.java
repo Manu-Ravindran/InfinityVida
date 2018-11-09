99c13223b5ded5142b7e548e63f577520af802c7
Manu Ravindran <44898436+Manu-Ravindran@users.noreply.github.com>
2018-11-09 18:41:01 +0530

    A simple Android app

---
+++ InfinityVida/app/src/main/java/com/omana/infinityvida/StartUp.java
@@ -0,0 +1,36 @@
+package com.omana.infinityvida;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartUp extends AppCompatActivity {
+
+    private static int SPLASH_TIME = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_up);
+
+
+        new Handler().postDelayed(new Runnable()
+        {
+
+            @Override
+
+            public void run()
+            {
+
+                Intent mySuperIntent = new Intent(StartUp.this, StartingScreenActivity.class);
+
+                startActivity(mySuperIntent);
+
+                finish();
+
+            }
+
+        }, SPLASH_TIME);
+    }
+}
